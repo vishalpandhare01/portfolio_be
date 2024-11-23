d91a1039877dce9af129f6a5f32e6a76d98cc8ca
vishal pandhare <vishal@cowhite.com>
2024-11-23 15:46:03 +0530

    cors added

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"github.com/vishalpandhare01/portfolio_be/initializer"
 	"github.com/vishalpandhare01/portfolio_be/internal"
@@ -18,10 +19,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Error in load env: ", err)
 	}
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:8000, http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept , Token , Authorization",
+	}))
 	port := os.Getenv("PORT")
 	initializer.ConnectDb()
 	migration.SetUpMigration()
 	internal.SetUpRoutes(app)
 	fmt.Println("🚀 Server running on Port: ", port)
-	app.Listen("0.0.0.0:" + port)
+	app.Listen(port)
 }
