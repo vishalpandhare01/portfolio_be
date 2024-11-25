f5e8ff4bba1b103d03d7ce68206c6eae28981852
vishal pandhare <vishal@cowhite.com>
2024-11-25 18:10:29 +0530

    frontend domain added

--- main.go
+++ main.go
@@ -14,13 +14,15 @@ import (
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		BodyLimit: 100 * 1024 * 1024, // Set global body size limit (100 MB)
+	})
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error in load env: ", err)
 	}
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:8000, http://localhost:3000 ,http://localhost:3001 ,http://localhost:3002",
+		AllowOrigins: "http://localhost:8000, http://localhost:3000 ,http://localhost:3001 ,http://localhost:3002 , https://myportfoliobuilder.vercel.app/ , https://nine-portfolio.vercel.app/",
 		AllowHeaders: "Origin, Content-Type, Accept , Token , Authorization",
 	}))
 	port := os.Getenv("PORT")
